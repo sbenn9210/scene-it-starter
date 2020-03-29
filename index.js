ad5e63eda24a301b1e6bffcf197d2bc366e4f8fb
Shawn Benny <sbenn9210@gmail.com>
2020-03-29 13:47:34 -0500

    Basic search functionality working

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 $(document).ready(function() {
-    $('.movies-container').html(renderMovies(movieData))
-})
+  document.getElementById('search-form').addEventListener('submit', function(e){
+    e.preventDefault()
 
-function renderMovies (movieArray) {
-     let finalHTML = movieArray.map(currentMovie => {
-        return  `
-        <div class="mr-5 mb-2 card movie" style="width: 18rem;">
-        <img class="card-img-top" src=${currentMovie.Poster} alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">${currentMovie.Title}</h5>
-          <p class="card-text">${currentMovie.Year}</p>
-          <a href="#" class="btn btn-primary">Add</a>
-        </div>
+  })
+
+let moviesContainer = document.querySelector('.movies-container')
+
+let input = document.getElementById('search-input')
+ input.addEventListener('input', function(e) {
+
+   moviesContainer.innerHTML = renderMovies(movieData, e.target.value.toUpperCase())
+ })
+
+function renderMovies (movieArray, selectedMovie) {
+  
+   let finalHTML = movieArray.map(currentMovie => {
+     if (currentMovie.Title.toUpperCase().indexOf(selectedMovie) > -1) {
+      
+      return  `
+      <div class="mr-5 mb-2 card movie" style="width: 18rem;">
+      <img class="card-img-top" src=${currentMovie.Poster} alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${currentMovie.Title}</h5>
+        <p class="card-text">${currentMovie.Year}</p>
+        <a href="#" class="btn btn-primary">Add</a>
       </div>
-        `
-        
-    })
-    
-    return finalHTML.join('')
-    
+    </div>
+      `
+     } 
+      
+  })
+  
+  return finalHTML.join('')
+  
 }
+
+})
